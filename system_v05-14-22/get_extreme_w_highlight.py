c190bbf4874809a211ef94adf0854aa23d58a879
Steve Li <lithafnium2000@gmail.com>
2022-08-05 15:28:28 -0400

    refactored return_extreme_values

--- system_v05-14-22/get_extreme_w_highlight.py
+++ system_v05-14-22/get_extreme_w_highlight.py
@@ -9,6 +9,9 @@ import tqdm
 from transformers import AdamW, get_linear_schedule_with_warmup
 import numpy as np
 from torch import nn
+from sklearn.metrics import auc, roc_curve
+import scipy as sp
+
 
 import shap
 
@@ -20,7 +23,7 @@ TOP_K = 100
 NUM_FOLD = 4
 bsize = 16
 NUM_STEPS = 2000
-
+PATIENCE = 5
 max_length = 128
 
 
@@ -117,6 +120,9 @@ def train(cv_dict):
     scheduler = get_linear_schedule_with_warmup(optimizer, 400, NUM_STEPS)
     tokenizer = AutoTokenizer.from_pretrained(pretrain_model)
     model.train(); 
+
+    auc_score = 0
+    no_improv = 0
     for step in tqdm.trange(NUM_STEPS):
         random.shuffle(train_data_dicts)
         input_texts = [d['input'] for d in train_data_dicts[:bsize]]
@@ -124,15 +130,30 @@ def train(cv_dict):
         labels = torch.tensor([d['label'] for d in train_data_dicts[:bsize]]).to(device)
         outputs = model(**inputs, labels=labels)
         loss = outputs['loss']
+        logits = outputs['logits']
+
         loss.backward()
         if step % 2 == 1:
             optimizer.step()
             scheduler.step()
             optimizer.zero_grad()
 
+        # early stopping criteria
+        if step % 10 == 0:
+            fpr, tpr, thresholds = roc_curve(labels.detach().cpu().numpy(), logits.detach().cpu().numpy()[:,1])
+            new_auc_score = auc(fpr, tpr)
+            print(new_auc_score)
+            if new_auc_score <= auc_score:
+                no_improv += 1
+            else:
+                no_improv = 0
+                auc_score = new_auc_score
+            
+            if no_improv >= PATIENCE:
+                break
     torch.save(model.state_dict(), "roberta-attention.pt")
+    return model, tokenizer
 
-        
 
 def evaluate(texts, use_shap: bool, model, tokenizer):
     model.eval()
@@ -164,9 +185,18 @@ def evaluate(texts, use_shap: bool, model, tokenizer):
             'highlights': highlights
         }
     else:
+        def predict(x):
+            tv = torch.tensor(
+                [tokenizer(v, padding=True, max_length=max_length, truncation=True).to(device) for v in x]
+            )
+            outputs = model(tv)[0].detach().cpu().numpy()
+            scores = (np.exp(outputs).T / np.exp(outputs).sum(-1)).T
+            val = sp.special.logit(scores[:,1]) # use one vs rest logit units
+            return val
+        
         all_logits, all_highlights = [], []
         cur_start = 0
-        explainer = shap.Explainer(model, tokenizer)
+        explainer = shap.Explainer(predict, tokenizer)
 
         while cur_start < len(texts):
             texts_ = texts[cur_start:cur_start + bsize]
@@ -174,70 +204,11 @@ def evaluate(texts, use_shap: bool, model, tokenizer):
             print(shap_values)
 
 
-def train_and_eval(cv_dict):
-    max_length = 128
-    train_data_dicts = list(chain(
-        [{'input': x, 'label': 1} for x in cv_dict['train_pos']], 
-        [{'input': x, 'label': 0} for x in cv_dict['train_neg']], 
-    ))
-    # model = RobertaForSequenceClassification.from_pretrained(pretrain_model).to(device)
-    # model = RoBERTaSeq().to(device)
-    model = RoBERTaSeqAttn().to(device)
-    no_decay = ['bias', 'LayerNorm.weight']
-    optimizer_grouped_parameters = [
-        {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
-         'weight_decay': 0.01},
-        {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
-    ]
-    optimizer = AdamW(optimizer_grouped_parameters, lr=5e-5)
-    scheduler = get_linear_schedule_with_warmup(optimizer, 400, NUM_STEPS)
-    tok = AutoTokenizer.from_pretrained(pretrain_model)
-    
-    for step in tqdm.trange(NUM_STEPS):
-        random.shuffle(train_data_dicts)
-        input_texts = [d['input'] for d in train_data_dicts[:bsize]]
-        inputs = tok(input_texts, return_tensors='pt', truncation=True, max_length=max_length, padding=True).to(device)
-        labels = torch.tensor([d['label'] for d in train_data_dicts[:bsize]]).to(device)
-        outputs = model(**inputs, labels=labels)
-        loss = outputs['loss']
-        loss.backward()
-        if step % 2 == 1:
-            optimizer.step()
-            scheduler.step()
-            optimizer.zero_grad()
-
-    def evaluate(texts):
-        all_logits, all_highlights = [], []
-        cur_start = 0
-        while cur_start < len(texts):
-            texts_ = texts[cur_start:cur_start + bsize]
-            inputs = tok(texts_, return_tensors='pt', truncation=True, max_length=max_length, padding=True).to(device)
-            model_output_dict = model(**inputs)
-            logits = lsm(model_output_dict['logits'].detach().cpu()).numpy().tolist()
-            all_highlights.extend(model_output_dict['highlight'])
-            all_logits.extend(logits)
-            cur_start += bsize
-        assert len(all_logits) == len(texts)
-        
-        all_spans = [get_spans(tok, text) for text in texts]
-        assert len(all_spans) == len(all_highlights)
-        for a, b in zip(all_spans, all_highlights):
-            assert len(a) == len(b) or len(a) >= max_length
-        
-        highlights = [
-            {s: h for s, h in zip(spans, highlights) if s != (0, 0)} 
-            for spans, highlights in zip(all_spans, all_highlights)
-        ]
-        
-        return {
-            'logits': np.array(all_logits),
-            'highlights': highlights
-        }
-    
-    pos_eval_dict = evaluate(cv_dict['test_pos'])
+def train_and_eval(cv_dict, use_shap):
+    model, tokenizer = train(cv_dict)
+    pos_eval_dict = evaluate(cv_dict['test_pos'], use_shap, model, tokenizer)
     pos_logits, pos_highlights = pos_eval_dict['logits'][:,1], pos_eval_dict['highlights']
-    
-    neg_eval_dict = evaluate(cv_dict['test_neg'])
+    neg_eval_dict = evaluate(cv_dict['test_neg'], use_shap, model, tokenizer)
     neg_logits, neg_highlights = neg_eval_dict['logits'][:,0], neg_eval_dict['highlights']
     
     return {
@@ -247,12 +218,14 @@ def train_and_eval(cv_dict):
         'test_neg_highlight': neg_highlights 
     }
 
-def return_extreme_values(pos, neg, train):
+
+
+def return_extreme_values(pos, neg, train: bool):
     pos2score, neg2score = {}, {}
     pos2highlight, neg2highlight = {}, {}
     
     for fold_idx, cv_dict in enumerate(cv(pos, neg, NUM_FOLD)):
-        test_scores = train_and_eval(cv_dict)
+        test_scores = train_and_eval(cv_dict, False)
         for pos_text, score, highlight in zip(cv_dict['test_pos'], test_scores['test_pos_scores'], test_scores['test_pos_highlight']):
             pos2score[pos_text] = score
             pos2highlight[pos_text] = highlight
--- system_v05-14-22/get_rep_from_cluster.py
+++ system_v05-14-22/get_rep_from_cluster.py
@@ -9,42 +9,42 @@ import tqdm
 
 from get_extreme_w_highlight import return_extreme_values
 
-def get_rep(pos: List[str], # a list of text samples from D_1
-             neg: List[str], # a list of text samples from D_0
-             pair: str='',
-             save_folder=None):
+
+def get_rep(
+    pos: List[str],  # a list of text samples from D_1
+    neg: List[str],  # a list of text samples from D_0
+    pair: str = "",
+    save_folder=None,
+):
     if save_folder is None:
-        save_folder = os.path.join('end2end_jobs', pair)
+        save_folder = os.path.join("end2end_jobs", pair)
         print(save_folder)
     if not os.path.exists(save_folder):
         os.mkdir(save_folder)
     else:
-        print('Folder %s exists' % save_folder)
+        print("Folder %s exists" % save_folder)
 
     # get samples that are representative of the differences between two distributions
-    extreme_vals = return_extreme_values(pos, neg)
-    pkl.dump(extreme_vals, open(os.path.join(save_folder, 'get_extreme_result.pkl'), 'wb')) 
+    extreme_vals = return_extreme_values(pos, neg, False)
+    pkl.dump(
+        extreme_vals, open(os.path.join(save_folder, "get_extreme_result.pkl"), "wb")
+    )
 
 
-if __name__ == '__main__':
-  with open("cluster.json", "r") as f:
-    clusters = json.load(f)
+if __name__ == "__main__":
+    with open("cluster.json", "r") as f:
+        clusters = json.load(f)
 
-  indexes = [f"{i}" for i in range(64)]
+    indexes = [f"{i}" for i in range(64)]
 
-  pairs = np.random.choice(indexes, size=(10, 2), replace=False)
+    pairs = np.random.choice(indexes, size=(10, 2), replace=False)
 
-  for (i, pair) in enumerate(tqdm.tqdm(pairs)):
-    print("pair:", i)
-    d0_index, d1_index = pair[0], pair[1]
+    for (i, pair) in enumerate(tqdm.tqdm(pairs)):
+        print("pair:", i)
+        d0_index, d1_index = pair[0], pair[1]
 
-    d0, d1 = clusters[d0_index], clusters[d1_index]
-    try: 
-      get_rep(pos=d0, neg=d1, pair=str())
-    except:
-      continue
-
-    
-
-
-  
+        d0, d1 = clusters[d0_index], clusters[d1_index]
+        try:
+            get_rep(pos=d0, neg=d1, pair=str())
+        except:
+            continue
