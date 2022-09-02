e41e7df1a24178e08c2de7ff545392e8bd5fb67a
Peter Zhang <petez@berkeley.edu>
2022-09-02 15:16:32 -0700

    Update test_tree.py

--- system_v05-14-22/test_tree.py
+++ system_v05-14-22/test_tree.py
@@ -2,13 +2,14 @@ from tree_d3 import D3TreeSystem
 import json
 
 if __name__ == '__main__':
-    d = json.load(open('../benchmark_sec_4/benchmark_ss.json'))[46]
-    pos = d['positive_samples']
-    neg = d['negative_samples']
-    pair = d['pair']
-    print(pair)
-    d3sys = D3TreeSystem()
-    d3sys.fit(pos,
-          neg,
-          pair,
-          depth=1)
+    dists = json.load(open('../benchmark_sec_4/benchmark_ss.json'))
+    for d in dists[70:75]:
+        pos = d['positive_samples']
+        neg = d['negative_samples']
+        pair = d['pair']
+        print(pair)
+        d3sys = D3TreeSystem()
+        d3sys.fit(pos,
+            neg,
+            pair,
+            depth=1)
