79924445141421197575bda585a4cb4134c1ee51
Peter Zhang <petez@berkeley.edu>
2022-10-15 16:28:19 -0700

    Update iterative_d3.py

--- system_v05-14-22/iterative_d3.py
+++ system_v05-14-22/iterative_d3.py
@@ -85,11 +85,10 @@ def predict_scores(pos,
 
 if __name__ == '__main__':
     import tqdm
-    distribution_pairs = json.load(open('../benchmark_sec_4/benchmark.json'))[10:]
+    distribution_pairs = json.load(open('../benchmark_sec_4/benchmark.json'))[17:]
 
     all_h2score = []
     for i, d in enumerate(tqdm.tqdm(distribution_pairs)):
-        print(d['pair'])
         h2score = iterative_d3(pos=d['positive_samples'], 
                            neg=d['negative_samples'], 
                            note='benchmark %d; can be anything, for logging purpose only' % i)
